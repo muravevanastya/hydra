1ff16a785b24e43ca15fbc04c9333f3e569e854e
Anastasia Muravyeva <muravevanastya@gmail.com>
2023-10-05 17:58:31 +0300

    made a slider for process page

--- script.js
+++ script.js
@@ -25,3 +25,32 @@ const servicesSwiper = new Swiper('.services__cards', {
     prevEl: '.services__cards-btn-prev',
   },
 });
+
+const techSwiper = new Swiper('.tech__companies', {
+  slidesPerView: 1,
+  loop: true,
+  breakpoints: {
+    840: {
+      slidesPerView: 4,
+    }
+  },
+  navigation: {
+    nextEl: '.tech__companies-btn-next',
+    prevEl: '.tech__companies-btn-prev',
+  },
+});
+
+const processSwiper = new Swiper('.process__container', {
+  slidesPerView: 1,
+  loop: true,
+  breakpoints: {
+    840: {
+      slidesPerView: 4,
+    }
+  },
+  navigation: {
+    nextEl: '.process__container-btn-next',
+    prevEl: '.process__container-btn-prev',
+  },
+  // spaceBetween: 20,
+});
